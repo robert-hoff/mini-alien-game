c4ffb4600f33ca40862fb6491068d7ad880cd5ea
Hoff Inc <hoff.rj@gmail.com>
2025-12-06 14:56:53 +0000

    maven template

---
+++ src/main/java/proj/MainClass.java
@@ -0,0 +1,14 @@
+package proj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainClass {
+
+  public static void main(String[] a) {
+    log.info("hello");
+  }
+
+
+  private static Logger log = LoggerFactory.getLogger(MainClass.class);
+}
